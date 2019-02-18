992b11d086e4d3cc92e4ba6c8f26b4255943a409
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2019-02-18 15:46:23 -0500

    Counter and Session Logging Middleware

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ app.use(
   })
 );
 
+// make sure to put this AFTER your session middleware, but BEFORE you send your response!
+app.use((req, res, next) => {
+  if (!req.session.counter) req.session.counter = 0;
+  console.log('counter', ++req.session.counter); // increment THEN log
+  next(); // needed to continue through express middleware
+});
+// place right after the session setup middleware
+app.use((req, res, next) => {
+  console.log('SESSION: ', req.session);
+  next();
+});
+
 app.get('/', (req, res, next) => {
   res.send('Hello');
 });
